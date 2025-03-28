70f80dbfa98ea22db46fefd032dbe14e06d7b732
Sandeepa <94948014+saniya20202@users.noreply.github.com>
2025-03-28 23:15:05 +0530

    Create MainPart.jsx
    added welcome text

---
+++ src/components/MainPart.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./MainPart.css";
+
+const MainPart = () => {
+    return (
+        <div className ="welcomeNote">
+            <h4>Hi i am </h4>
+            <p><h3>Sandipa Senadhira</h3></p>
+        </div>
+    );
+};
+
+export default MainPart;
