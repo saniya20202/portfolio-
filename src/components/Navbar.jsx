46afe3814d8451434464e9b18f1810c07b693b4c
Sandeepa <94948014+saniya20202@users.noreply.github.com>
2025-04-18 11:45:09 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,23 @@ import "./Navbar.css";
 
 const Navbar = () => {
     return (
-        <nav>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Services</a></li>     
-                <li><a href="#">Contact</a></li>            
-            </ul>
-            <div className="profile">
-                <h4>Profile</h4>               
-            </div>
-        </nav>
-    )
-}
+        <>
+            <input type="checkbox" id="menu-toggle" />
+            <label htmlFor="menu-toggle" className="menu-btn">☰</label>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="AboutMe.jsx">About me</a></li>
+                    <li><a href="#">Skills</a></li>     
+                    <li><a href="#">Projects</a></li>   
+                    <li><a href="#">Contact Me</a></li>         
+                </ul>
+                <div className="profile">
+                    <h4>Profile</h4>               
+                </div>
+            </nav>
+        </>
+    );
+};
 
 export default Navbar;
