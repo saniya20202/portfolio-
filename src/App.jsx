d70a2cae3536c03d91d877785379397b0cf517f5
Sandeepa <94948014+saniya20202@users.noreply.github.com>
2025-04-18 11:47:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import Navbar from './components/navbar'; 
+import Navbar from './components/Navbar'; 
 import MainPart from './components/MainPart';  
 import AboutMe from './components/AboutMe';
 import SkillsSection from './components/SkillsSection';
-
+import Projects from './components/Projects';
+import ContactMe from './components/ContactMe';
 
 const App = () => {
   return (
     <div className='App'>
-      <Navbar />     
-      <MainPart /> 
-      <AboutMe />   
+      <Navbar />
+      <MainPart />
+      <AboutMe />
       <SkillsSection />
+      
+      
+
+  
+      <Projects />
+
+      <ContactMe />
+
       
     </div>
   );
