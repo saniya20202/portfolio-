e39cf9f5aed8f9309dfed67748468f450eb10f37
Sandeepa <94948014+saniya20202@users.noreply.github.com>
2025-04-20 19:11:34 +0530

    added splash screen and update content

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar'; 
 import MainPart from './components/MainPart';  
 import AboutMe from './components/AboutMe';
 import SkillsSection from './components/SkillsSection';
 import Projects from './components/Projects';
 import ContactMe from './components/ContactMe';
+import SplashScreen from './components/SplashScreen'; // Import the new component
+
+
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <div className='App'>
-      <Navbar />
-      <MainPart />
-      <AboutMe />
-      <SkillsSection />
+      {/* Show splash screen while loading */}
+      {isLoading && <SplashScreen onLoadingComplete={() => setIsLoading(false)} />}
       
-      
-
-  
-      <Projects />
-
-      <ContactMe />
-
-      
+      {/* Main app content - hidden while splash is showing */}
+      <div style={{ visibility: isLoading ? 'hidden' : 'visible' }}>
+        <Navbar />
+        <MainPart />
+        <AboutMe />
+        <SkillsSection />
+        <Projects />
+        <ContactMe />
+      </div>
     </div>
   );
 }
